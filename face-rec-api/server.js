978ee279d4e6a44c424003fc6727b31b56d876fd
usmanjchaudhry <uidman72@gmail.com>
2023-04-10 07:01:30 -0700

    updated port

--- face-rec-api/server.js
+++ face-rec-api/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt-nodejs')
-const cors = require('cors')
+const cors = require('cors');
+const { dblClick } = require('@testing-library/user-event/dist/click');
 const knex = require('knex')({
     client: 'pg',
     connection: {
@@ -53,67 +54,95 @@ app.get('/', (req, res)=>{
 }) 
 
 app.post('/signin',(req,res)=>{
-    if(req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password){
-            res.json(database.users[0]);
-
-        }else{
-            res.status(400).json('error logging in')
-        }
+    const {email, password} = req.body;
+    if(!email || !password){
+        res.status(400).json('incorrect form submission')
+    }
+knex.select('email','hash').from('login')
+.where('email', '=', req.body.email)
+    .then(data =>{
+       const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+       console.log(isValid)
+       if(isValid){
+       return knex.select('*').from('users')
+        .where('email', '=', req.body.email)
+        .then(user =>{
+            res.json(user[0])
+        })
+        .catch(err => res.status(400).json('unable to get user'))
+       }else{
+       res.status(400).json('wrong credentials')
+       }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
 })
 
 
-app.post('/register',(req,res)=>{
-    const {email,name,password} = req.body  
-  knex('users').insert({
-    email:email,
-    name:name,
-    joined: new Date()
-
-
-  }).then(response =>{
-    res.json(response)
-
-  })
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    if(!email || !name || !password){
+        res.status(400).json('incorrect form submission')
+    }
+    const hash = bcrypt.hashSync(password);
+  
+    knex.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login') 
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date()
+          })
+      .then(user => {
+        res.json(user[0]);
+      })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
 })
+      .catch(err => {
+        res.status(400).json('unable to register');
+      });
+    });
 
 
 app.get('/profile/:id', (req,res)=>{
-    const {id} = req.params;
-    let found = false;
-    database.users.forEach(user =>{
-        if(user.id === id){
-            found = true;
-            return res.json(user)
+    const { id } = req.params;
+    knex.select('*').from('users').where({id})
+    .then(user =>{
+        if(user.length){
+            res.json(user[0])
+        }else{
+            res.status(400).json('Not found')
         }
     })
-    if(!found){
-        res.status(400).json('not found')
-    }
+    .catch(err => res.status(400).json('error getting user'))
 })
+  
+
+
+    app.put('/image', (req, res) => {
+        const { id } = req.body;
+        knex('users')
+          .where('id', '=', id)
+          .increment('entries', 1)
+          .returning('entries')
+          .then(entries => {
+            res.json(entries[0]);
+          })
+          .catch(err => res.status(400).json('unable to get count'));
+      });
+      
 
 
 
-app.put('/image',(req,res)=>{
-    
-    const {id} = req.body;
-    let found = false;
-    database.users.forEach(user =>{
-        if(user.id === id){
-            found = true;
-            user.entries++
-            return res.json(user.entries)
-        }
-    })
-    if(!found){
-        res.status(400).json('not found')
-    }
-
-})
-
-
-
-
-app.listen(3000, ()=>{
-    console.log('App is running on port 3000')
+app.listen( process.env.PORT || 3000, ()=>{
+    console.log(`App is running on port ${process.env.PORT}`)
 })
--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -9,14 +9,13 @@ const Rank = ({ user }) => {
 
   console.log(user.entries)
 
-
   return (
     <div>
       <div className='white f3'>
         {`${user.name}, your current entry count is...`}
       </div>
       <div className='white f1'>
-        {entries}
+        {entries && entries.entries}
       </div>
     </div>
   );
